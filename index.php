069dea1fe16c0a42ad4c9eac6066e1edeb867752
raj laxmi singh <112322594+rmhere55@users.noreply.github.com>
2023-12-10 16:52:41 +0000

    object , class , inherteance,constructer,destucre

--- index.php
+++ index.php
@@ -1,15 +1,62 @@
 <?php
+// class
+// class fruit {
+// 	public $name;
+// 	public $color;
+// 	function __construct($name,$color){
+// 		$this->name = $name;
+// 		$this->color=$color;
 
-/*----------------------------------------------------------------------------------------
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License. See LICENSE in the project root for license information.
- *---------------------------------------------------------------------------------------*/
+// 	}
+// 	public function intro (){
+// 		echo "a {$this->name} and also a {$this->color}";
+// 	}
+	// function get_name(){
+	// 	return $this->name;
+	// }
+	// function get_color(){
+	// 	return $this->color;
+	// }
+// }
+//  class Charry extends fruit{
+// 	public function message(){
+// 		echo "is cheery a fruit ";
+// 	}}
+// 	$cheery = new Charry("charry", "red");
+// 	$cheery->message();
+// 	$cheery->intro();
 
-function sayHello($name) {
-	echo "Hello $name!";
-}
+// }
+// class Cherry extends fruit{
+// 	public $weig;
+// 	public function __construct($name,$color,$weig)
+// 	{
+// 		$this->name =$name;
+// 		$this->color =$color;
+// 		$this->weig =$weig;
+// 	}
+// 	public function intro()
+// 	{
+// 		echo"this is fruit is {$this->name}, the color is {$this->color} or at the end of the day {$this->weig}";
+// 	}
+// }
+ 
+
+// $cheery = new Cherry("cherry", "bright pink",20);
+// $cheery->intro();
+
+// $stwarberry = new fruit("stwarberry","red");
+// echo $stwarberry->get_name();
+// echo $stwarberry->get_color();
+
+// function __destruct(){
+// 	echo"the fruit is {$this->name} ant the color is {$this->color}";
+// }
 
 ?>
+
+
+
 
 <html>
 	<head>
@@ -18,10 +65,12 @@ function sayHello($name) {
 	<body>
 		<?php 
 		
-		sayHello('remote world');
-			
-		phpinfo(); 
-			
+		 echo "hyyy hlooo";	
+
+		//  destracter
+
+		   
+		
 		?>
 	</body>
 </html>
